cc799171c3060a2903374c5fbb233ca39d701d0e
Issac Kelly <issac@issackelly.com>
2024-01-04 10:24:58 -0800

    Version bump

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     bowtie = {
       source  = "bowtieworks/bowtie"
-      version = "0.5.0"
+      version = "0.5.1"
     }
     checkmate = {
       source = "tetratelabs/checkmate"
